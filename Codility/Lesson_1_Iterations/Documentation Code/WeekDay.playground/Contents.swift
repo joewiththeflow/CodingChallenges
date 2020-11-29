c698686ebbd5a48ae02e5676200963bc60834cc0
joewiththeflow <35314103+joewiththeflow@users.noreply.github.com>
2020-11-29 21:00:32 +0000

    Lesson 1 - Swift

---
+++ Codility/Lesson_1_Iterations/Documentation Code/WeekDay.playground/Contents.swift
@@ -0,0 +1,28 @@
+
+
+// Lesson 1: Iterations - Loop over an array and arrayList and print out each value
+
+// Use String array
+func daysOfWeek() {
+    let daysOfWeek = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+    
+    // Use a for loop for an array of strings
+    for day in daysOfWeek {
+        print(day)
+    }
+}
+
+// Test using String Array
+daysOfWeek();
+
+
+
+// Expected Output
+
+// Monday
+// Tuesday
+// Wednesday
+// Thursday
+// Friday
+// Saturday
+// Sunday
